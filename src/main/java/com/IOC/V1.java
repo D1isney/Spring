8a6f5926d3677d2c55b33e2ac46f2052464319af
D1isney <2497050447@qq.com>
2022-05-20 23:59:44 +0800

    创建IOC容器
    Commit

--- src/main/java/com/IOC/V1.java
+++ src/main/java/com/IOC/V1.java
@@ -10,7 +10,7 @@ public class V1 {
         //通过反射机制创建
         Student student = context.getBean(Student.class);
         //返回的是Object型 需要强转
-//        Student student1 = (Student) context.getBean("student");
+//        Student_AOP student1 = (Student_AOP) context.getBean("student");
         System.out.println(student);
 //        System.out.println(student1);
         //单例 默认作用域是单例模式
