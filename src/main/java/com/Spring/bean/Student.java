ef303b68d1deb49beb5693330a1db38a7369619c
D1isney <2497050447@qq.com>
2022-05-20 23:14:41 +0800

    注入依赖
    Commit

--- src/main/java/com/Spring/bean/Student.java
+++ src/main/java/com/Spring/bean/Student.java
@@ -3,40 +3,53 @@ package com.Spring.bean;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Map;
 
 @ToString
 public class Student {
     String name;
     int age;
-    Card card;
-    List<String> list;
+//    Card card;
+//    List<String> list;
+//    Map<String,Integer>map;
 
-    public void setName(String name ){
+//    public void setName(String name ){
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age){
+//        this.age = age;
+//    }
+
+//    public void setCard(Card card) {
+//        this.card = card;
+//    }
+
+//    public void setMap(Map<String, Integer> map) {
+//        this.map = map;
+//    }
+//
+//    public void setList(List<String> list) {
+//        this.list = list;
+//    }
+//
+//    public void init(){
+//        System.out.println("我是初始化方法");
+//    }
+//
+//    public Student(){
+//        System.out.println("我是Student");
+//    }
+    public Student(String name,int age){
         this.name = name;
-    }
-
-    public void setAge(int age){
         this.age = age;
     }
 
-    public void setCard(Card card) {
-        this.card = card;
+    public Student(String name){
+        this.name=name;
     }
 
-
-    public void setList(List<String> list) {
-        this.list = list;
-    }
-
-    public void init(){
-        System.out.println("我是初始化方法");
-    }
-
-    public Student(){
-        System.out.println("我是Student");
-    }
-
-    public void destory(){
-        System.out.println("我是销毁方法");
-    }
+//    public void destory(){
+//        System.out.println("我是销毁方法");
+//    }
 }
