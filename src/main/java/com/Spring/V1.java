e067557768c3cd06036955389edf7c28382a4dcd
D1isney <2497050447@qq.com>
2022-05-20 22:48:26 +0800

    创建IOC容器、配置文件bean的写入
    Commit

--- src/main/java/com/Spring/V1.java
+++ src/main/java/com/Spring/V1.java
@@ -17,7 +17,6 @@ public class V1 {
         // 可以个beans.xml上面更改模式（原型模式）scope="prototype"
 //        System.out.println(student == student1);
 //        System.out.println(student.equals(student1));
-        
         context.close();
 //        depends-on="card"
         //表示card先初始化
